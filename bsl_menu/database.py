165b0af2937c12adc394def3cc9f6f181cd5e94f
Anton <antongb09@gmail.com>
2023-12-29 20:01:43 +0000

    delete local db

--- bsl_menu/database.py
+++ bsl_menu/database.py
@@ -1,14 +1,14 @@
 from typing import Annotated
 from fastapi import Depends
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, pool
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./bsl_menu.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, poolclass=pool.NullPool
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
