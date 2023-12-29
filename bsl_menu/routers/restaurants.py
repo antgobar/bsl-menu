ae79000b411f04f7180cc163b1d150b9a5ba0e11
Anton <antongb09@gmail.com>
2023-12-29 14:26:29 +0000

    remove some url_Fors

--- bsl_menu/routers/restaurants.py
+++ bsl_menu/routers/restaurants.py
@@ -22,7 +22,7 @@ router = APIRouter(prefix=f"/{ENDPOINT}", tags=[ENDPOINT])
 
 @router.get("/register")
 async def register_restaurant_page(request: Request):
-    return templates.TemplateResponse("register_restaurant.html", {"request": request})
+    return templates.TemplateResponse("restaurant_register.html", {"request": request})
 
 
 @router.post('/register', response_class=HTMLResponse)
