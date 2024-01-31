ad3b74d3a1acd49d9bb02831ee55dc9d6a7ee8a2
antgobar <antongb09@gmail.com>
2024-01-31 21:56:47 +0000

    cleanup template functionality

--- bsl_menu/templates.py
+++ bsl_menu/templates.py
@@ -2,13 +2,11 @@ import os
 
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
+from jinja2 import pass_context
 
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 templates = Jinja2Templates(directory=os.path.join(current_directory, "templates"))
-
-
-from jinja2 import pass_context
 
 
 @pass_context
