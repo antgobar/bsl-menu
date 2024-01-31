2dfcd9feab70563fd0562fcf1e2acb11e2853cdc
antgobar <antongb09@gmail.com>
2024-01-31 21:03:06 +0000

    modified url_for

--- bsl_menu/templates.py
+++ bsl_menu/templates.py
@@ -1,7 +1,23 @@
 import os
 
+from fastapi import Request
 from fastapi.templating import Jinja2Templates
 
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 templates = Jinja2Templates(directory=os.path.join(current_directory, "templates"))
+
+
+from jinja2 import pass_context
+
+
+@pass_context
+def urlx_for(context: dict, name: str, **path_params: any) -> str:
+    request: Request = context['request']
+    http_url = request.url_for(name, **path_params)
+    if scheme := request.headers.get('x-forwarded-proto'):
+        return http_url.replace(scheme=scheme)
+    return http_url
+
+
+templates.env.globals['url_for'] = urlx_for
