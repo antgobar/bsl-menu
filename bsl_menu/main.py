4516a01b48aec8648c5f7d672ba67cda0bb4bd9d
antgobar <antongb09@gmail.com>
2024-01-31 21:13:56 +0000

    corss

--- bsl_menu/main.py
+++ bsl_menu/main.py
@@ -1,6 +1,7 @@
 import os
 
-from fastapi import FastAPI, Request, Form, Depends, HTTPException
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
@@ -11,6 +12,7 @@ from .dummy_setup import dummy_setup
 from . import routers
 from .templates import templates
 
+
 app = FastAPI()
 app.include_router(routers.restaurants.router)
 app.include_router(routers.visuals.router)
@@ -20,6 +22,14 @@ app.include_router(routers.search.router)
 current_directory = os.path.dirname(os.path.realpath(__file__))
 static_files = StaticFiles(directory=os.path.join(current_directory, "static"))
 app.mount("/static", static_files, name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", response_class=HTMLResponse)
